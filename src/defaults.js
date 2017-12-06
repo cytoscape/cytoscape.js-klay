2b471679e92fa31bf457392db3a0c635bad919af
Max Franz <maxkfranz@gmail.com>
2017-12-06 15:41:08 -0500

    Update scaffolding
    - Add Zenodo badge
    - Fixes and updates demo to be consistent with other extension demos
    - List Klay as dependency in readme
    - Port old files to new scaffolding and update code to use cjs

--- src/defaults.js
+++ src/defaults.js
@@ -1,63 +1,4 @@
-cytoscape-klay
-================================================================================
-
-[![DOI](https://zenodo.org/badge/82093643.svg)](https://zenodo.org/badge/latestdoi/82093643)
-
-## Description
-
-The Klay layout algorithm for Cytoscape.js ([demo](https://cytoscape.github.io/cytoscape.js-klay))
-
-This discrete layout creates good results for most graphs and it supports compound nodes.
-
-
-## Dependencies
-
- * Cytoscape.js ^3.2.0
- * Klay ^0.4.1
-
-
-## Usage instructions
-
-Download the library:
- * via npm: `npm install cytoscape-klay`,
- * via bower: `bower install cytoscape-klay`, or
- * via direct download in the repository (probably from a tag).
-
-Import the library as appropriate for your project:
-
-ES import:
-
-```js
-import cytoscape from 'cytoscape';
-import klay from 'cytoscape-klay';
-
-cytoscape.use( klay );
-```
-
-CommonJS require:
-
-```js
-let cytoscape = require('cytoscape');
-let klay = require('cytoscape-klay');
-
-cytoscape.use( klay ); // register extension
-```
-
-AMD:
-
-```js
-require(['cytoscape', 'cytoscape-klay'], function( cytoscape, klay ){
-  klay( cytoscape ); // register extension
-});
-```
-
-Plain HTML/JS has the extension registered for you automatically, because no `require()` is needed.
-
-
-## API
-
-```js
-var options = {
+let defaults = {
   nodeDimensionsIncludeLabels: undefined, // Boolean which changes whether label dimensions are included when calculating node dimensions (default true)
   fit: true, // Whether to fit
   padding: 20, // Padding on fit
@@ -115,29 +56,4 @@ var options = {
   priority: function( edge ){ return null; }, // Edges with a non-nil value are skipped when geedy edge cycle breaking is enabled
 };
 
-cy.layout( options ).run();
-```
-
-
-## Build targets
-
-* `npm run test` : Run Mocha tests in `./test`
-* `npm run build` : Build `./src/**` into `cytoscape-klay.js`
-* `npm run watch` : Automatically build on changes with live reloading (N.b. you must already have an HTTP server running)
-* `npm run dev` : Automatically build on changes with live reloading with webpack dev server
-* `npm run lint` : Run eslint on the source
-
-N.b. all builds use babel, so modern ES features can be used in the `src`.
-
-
-## Publishing instructions
-
-This project is set up to automatically be published to npm and bower.  To publish:
-
-1. Build the extension : `npm run build`
-1. Commit the build : `git commit -am "Build for release"`
-1. Bump the version number and tag: `npm version major|minor|patch`
-1. Push to origin: `git push && git push --tags`
-1. Publish to npm: `npm publish .`
-1. If publishing to bower for the first time, you'll need to run `bower register cytoscape-klay https://github.com/cytoscape/cytoscape.js-klay.git`
-1. [Make a new release](https://github.com/cytoscape/cytoscape.js-klay/releases/new) for Zenodo.
+module.exports = defaults;
